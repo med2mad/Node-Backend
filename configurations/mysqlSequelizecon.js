a9e8d7d734ab3699eccc764cbbcc99dd4f17a841
watawata <mohamed.leghdaich@gmail.com>
2024-03-25 20:04:04 +0000

    make vue build

--- configurations/mysqlSequelizecon.js
+++ configurations/mysqlSequelizecon.js
@@ -1,11 +1,13 @@
 const Sequelize = require('sequelize');
 const {app} = require('./expressapp');
 
-const sequelizeMysql = new Sequelize(process.env.mysql_database, process.env.mysql_user, process.env.mysql_password, {
-    host:process.env.mysql_host,
-    dialect:'mysql',
-    logging: false, //no logs in console
-});
+const sequelizeMysql = new Sequelize(process.env.mysql_database, process.env.mysql_user, process.env.mysql_password, 
+    {
+        host: process.env.mysql_host,
+        dialect: 'mysql',
+        logging: false, //no logs in console
+    }
+);
 
 sequelizeMysql.authenticate() //test connection made using "new Sequelize" before start listening
 .then(()=>{
