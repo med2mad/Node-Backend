73c391edd50558fc920ad3493ecf1895f6d83435
watawata <mohamed.leghdaich@gmail.com>
2024-07-03 00:04:45 +0100

    debug

--- 4 - routers/mongoose.js
+++ 4 - routers/mongoose.js
@@ -4,7 +4,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 router.use(mongoSanitize());
 
 //controller functions
-const {getAll, add, edit, remove} = require('../3 - controllers/mongoose');
+const {get, post, put, remove} = require('../3 - controllers/mongoose');
 
 //API endpoints
 router.get('/', get);
