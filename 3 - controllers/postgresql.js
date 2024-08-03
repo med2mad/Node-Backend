f3cb312c00da73d7649cf4043397c68a5ce09778
watawata <mohamed.leghdaich@gmail.com>
2024-08-03 02:27:53 +0100

    fix postgres pagination debug

--- 3 - controllers/postgresql.js
+++ 3 - controllers/postgresql.js
@@ -1,24 +1,22 @@
-const {Op,fn,col} = require('sequelize');
+const {Op} = require('sequelize');
 const Profile = require('../2 - models/Postgres');
 
 module.exports.
-get = async (req, res)=>{
+get = (req, res)=>{
     let whereClause = {};
     if (req.query.name) {whereClause.name = {[Op.like]:'%'+req.query.name+'%'};}
     if (req.query.age) {whereClause.age = req.query.age;}
     const limit = (req.query.limit?req.query.limit:0);
     const offset = (req.query.page?(req.query.page-1)*limit:0);
-    
-    let count = await Profile.findAll({where:whereClause, attributes: [[fn('count', col('_id')), 'total']]});
 
-    Profile.findAll({
+    Profile.findAndCountAll({
         where: whereClause,
         limit: parseInt(limit),
         offset:parseInt(offset),
         order: [['_id', 'DESC']],
     })
     .then((data)=>{
-        res.json({"rows":data, "total":count});
+        res.json({"rows":data.rows, "total":data.count});
     });
 };
 
