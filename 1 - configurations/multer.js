e6983c0676ffb6e108c0422f21744d10ebfbda2e
watawata <mohamed.leghdaich@gmail.com>
2024-09-12 20:39:49 +0100

    multer config

--- 1 - configurations/multer.js
+++ 1 - configurations/multer.js
@@ -1,13 +1,13 @@
 const multer = require('multer');
 const fs = require('fs');
 // const d = __dirname+'/../../public/uploads';
-const d = "C:/Users/MED/Desktop/AJAX Paradise/public/uploads/";
-// const d = "http://localhost:8080/uploads/";
+// const d = "C:/Users/MED/Desktop/AJAX Paradise/public/uploads/";
+const d =  '/webProjects/AJAX School/vue front/public/uploads/'; //this is consedered "localhost/webprojects", without the initial slash (/) the adress is relative (webProjects folder is created in this app)
 
 const storg = multer.diskStorage({destination: (req,file,callback)=>{ fs.mkdirSync(d, {recursive:true});
                                                                       callback(null, d);
                                                                     }, 
-                                  filename: (req,file,callback)=>{ callback(null, file.originalname+Date.now()); //what to parse in req.file.filename
+                                  filename: (req,file,callback)=>{ callback(null, Date.now()+'_'+file.originalname); //what to parse in req.file.filename
                                                                   }
                                 });
 
